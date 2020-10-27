f38b46d6179b25c7bc1b6ad0b300dfc96cf89070
Gharin Pautz <gharinpautz@gmail.com>
2020-10-26 17:41:53 -0700

    Initial Commit

---
+++ CPSC315PA6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPSC315PA6
+//
+//  Created by Gharin Pautz on 10/26/20.
+//  Copyright © 2020 Gharin Pautz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
