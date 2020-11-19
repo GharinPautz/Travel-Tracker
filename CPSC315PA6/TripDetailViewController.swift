99d7ae8efd489d2eefca0dcc167b9f79c3a55557
Gharin Pautz <gharinpautz@gmail.com>
2020-11-18 17:32:27 -0800

    images show on trip details page

--- CPSC315PA6/TripDetailViewController.swift
+++ CPSC315PA6/TripDetailViewController.swift
@@ -63,7 +63,15 @@ class TripDetailViewController: UIViewController {
             endDateLabel.text = "End Date: \(dateFormatter.string(from: trip.endDate!))"
             
             if let imageName = trip.imageFileName {
-                tripImageView.image = UIImage(named: imageName)
+                let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                // userDomainMask refers to the user's home directory
+                let fileURL = documentsDirectoryURL.appendingPathComponent(imageName).appendingPathExtension("jpeg")
+                           
+                let imageFromDisk = UIImage(contentsOfFile: fileURL.path)
+                           
+                if imageFromDisk != nil {
+                    tripImageView.image = imageFromDisk
+                }
             }
         }
     }
